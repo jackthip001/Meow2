8a36b2567e2bee89cc9316ed4fdbf644a440b01c
FelisDJ <J_Jackthip@hotmail.com>
2014-12-01 00:09:56 +0700

    cat & man can move

--- elements.py
+++ elements.py
@@ -1,40 +1,67 @@
 import pygame
 from pygame.locals import *
+from math import *
 
 rockImage = pygame.image.load("FlintStone.png")
 
 class ObjectRock(object):
-    
-    def __init__(self, radius, color, pos, speed = 3):
-        (self.x, self.y) = pos
-        self.radius = radius
-        self.color = color
+
+    def __init__(self, player, speed = 3):
+        # (self.x, self.y) = pos
         self.speed = speed
+        self.space = False
+        self.checkDistance = 0
+        self.player = player
+        self.force = 0
 
     def render(self, surface):
         self.image = pygame.transform.scale(rockImage, (int(30), int(30)))
         pos = (self.x, self.y)
         surface.blit(self.image, pos)
 
-    def setPos(self, player):
-        if player == "CAT":
-            (self.x, self.y) = (100, 290)
-        elif player == "MAN":
-            (self.x, self.y) = (499, 290)
+    def update(self):
+        self.force += 5
 
-    def move(self, player):
-        
-        if player == "CAT":
-            pass
-        elif player == "MAN":
-            pass
+    def reset(self):
+        self.force = 0
+        self.checkDistance = 0
 
+    def setPos(self, posX):
+        if self.player == "CAT":
+            (self.x, self.y) = (posX + 50, 290)
+        if self.player == "MAN":
+            (self.x, self.y) = (posX - 1, 290)
+
+    def move(self):
+        if self.checkDistance < self.force:
+            self.moveY()
+            if self.player == "CAT":
+                self.moveX_cat()
+            if self.player == "MAN":
+                self.moveX_man()
+            self.checkDistance += speed
+        else:
+            self.reset()
+
+    def moveX_man(self):
+        self.x -= self.speed
+
+    def moveX_cat(self):
+        self.x += self.speed
+
+    def moveY(self, theta = pi/4):
+        speedY = math.tan(theta) * self.speed
+        if self.x < force/2:
+            self.y += speedY
+        else:
+            self.y -= speedY
 
         
 ######## Players ########
 
 manImage = pygame.image.load("playerMan.png")
 meowImage = pygame.image.load("playerCat.png")
+
 
 class PlayerMan(object):
 
@@ -43,6 +70,13 @@ class PlayerMan(object):
         self.size = size
         self.hp = 3
         self.image = pygame.transform.scale(manImage, (int(self.size), int(self.size)))
+        self.round = False
+   
+    def is_round_false(self):
+        self.round = False
+
+    def is_round_true(self):
+        self.round = True
 
     def move_left(self):
         self.posX -= 5
@@ -61,9 +95,15 @@ class PlayerMeow(object):
         self.size = size
         self.hp = 3
         self.image = pygame.transform.scale(meowImage, (int(self.size), int(self.size)))
+        self.round = False
+
+    def is_round_false(self):
+        self.round = False
+
+    def is_round_true(self):
+        self.round = True
 
     def move_left(self):
-
         self.posX -= 5
 
     def move_right(self):
--- main.py
+++ main.py
@@ -12,19 +12,39 @@ class MeowGame(SimpleGame):
         super(MeowGame, self).__init__('Meow')
         self.man = PlayerMan(pos = (500, 320))
         self.cat = PlayerMeow(pos = (50, 320))
-        self.rock = ObjectRock(radius = 5, pos = (100, 290),color = MeowGame.WHITE)
+        self.rock_man = ObjectRock(player = "MAN")
+        self.rock_cat = ObjectRock(player = "CAT")
 
     def init(self):
         super(MeowGame, self).init()
 
     def update(self):
         if self.is_key_pressed(K_LEFT):
-            self.cat.move_left()
+            self.man.move_left()
         elif self.is_key_pressed(K_RIGHT):
+            self.man.move_right()
+
+        if self.is_key_pressed(K_SPACE):
+            self.rock_man.update()
+            self.man.is_round_true()
+
+        if self.is_key_pressed(K_a):
+            self.cat.move_left()
+        elif self.is_key_pressed(K_d):
             self.cat.move_right()
+        
+        if self.is_key_pressed(K_LSHIFT):
+            self.rock_cat.update()
+            self.cat.is_round_true()
+
 
     def render(self, surface):
-        self.rock.render(surface)
+        if self.cat.round == True:
+            self.rock_cat.setPos(self.cat.posX)
+            self.rock_cat.render(surface)
+        if self.man.round == True:
+            self.rock_man.setPos(self.man.posX)
+            self.rock_man.render(surface)
         self.man.render(surface)
         self.cat.render(surface)
 
